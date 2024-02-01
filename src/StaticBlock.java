4c7992daf536abae707b845e8c12b0db8b30cd00
ShankarShivagangePrakash <s.p.shankar.na@gmail.com>
2024-02-01 23:15:22 +0530

    Hello world and static block java program

---
+++ src/StaticBlock.java
@@ -0,0 +1,14 @@
+public class StaticBlock {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    static{
+        System.out.println("Static block 1");
+    }
+
+    static{
+        System.out.println("Static block 1");
+    }
+
+}
