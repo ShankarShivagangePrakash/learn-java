7420eae216377dc3171bc8bcafacff1959404a8d
ShankarShivagangePrakash <s.p.shankar.na@gmail.com>
2024-02-02 13:59:49 +0530

    command line arguments

---
+++ src/A15_Command_Line_Arguments.java
@@ -0,0 +1,8 @@
+public class A15_Command_Line_Arguments {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
