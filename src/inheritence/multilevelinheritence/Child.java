ecb37cdbf623f56056b742a3705536e3249570bc
ShankarShivagangePrakash <s.p.shankar.na@gmail.com>
2024-02-02 21:29:41 +0530

    memory allocation in inheritence

--- src/inheritence/multilevelinheritence/Child.java
+++ src/inheritence/multilevelinheritence/Child.java
@@ -2,6 +2,10 @@ package inheritence.multilevelinheritence;
 
 public class Child extends Parent{
 
+    Child() {
+        System.out.println("Child Constructor " + this);
+    }
+
     public void f2() {
         System.out.println("Inside f2()");
     }
--- src/inheritence/multilevelinheritence/Parent.java
+++ src/inheritence/multilevelinheritence/Parent.java
@@ -2,6 +2,10 @@ package inheritence.multilevelinheritence;
 
 public class Parent {
 
+    Parent() {
+        System.out.println("Parent Constructor " + this);
+    }
+
     public void f1() {
         System.out.println("Inside f1()");
     }
