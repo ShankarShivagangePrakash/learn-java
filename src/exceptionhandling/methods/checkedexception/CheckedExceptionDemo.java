c2ce7d59548a94f5af3b91cb3004b582de7ba64a
ShankarShivagangePrakash <s.p.shankar.na@gmail.com>
2024-02-04 22:07:32 +0530

    throws keyword demo

--- src/exceptionhandling/methods/checkedexception/CheckedExceptionDemo.java
+++ src/exceptionhandling/methods/checkedexception/CheckedExceptionDemo.java
@@ -1,16 +1,28 @@
 package exceptionhandling.methods.checkedexception;
 
-import java.io.File;
+import packages.p2.C;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class CheckedExceptionDemo {
 
+    public void readFile() throws FileNotFoundException {
+        // Below line can cause file not found exception, instead of handling that exception inside the class.
+        // We specify that this exception may be thrown from this class, whoever calls this method has to handle that exception
+        // we achieve this using throws keyword
+        FileInputStream file = new FileInputStream("");
+    }
+
     public static void main(String[] args) {
 
+        CheckedExceptionDemo checkedExceptionDemo = new CheckedExceptionDemo();
         //If file not found at the specified location of argument passed while creating object, it will throw file not found exception
         try {
-            FileInputStream file = new FileInputStream("");
+            //Here we are handling exception which could be thrown by readFile(),
+            // we can either handle it with catch block or
+            // add throws exceptionName to method signature, JVM will throw that exception if occurs.
+            checkedExceptionDemo.readFile();
         } catch (FileNotFoundException e) {
             System.out.println("File not found exception");
         }
