fdd81b25596b819daecf204298e507c162e860c8
ShankarShivagangePrakash <s.p.shankar.na@gmail.com>
2024-02-10 22:45:02 +0530

    initating garbage colection by creating lot of unreachable objects

--- src/garbagecollection/GarbageCollectionDemo.java
+++ src/garbagecollection/GarbageCollectionDemo.java
@@ -2,21 +2,25 @@ package garbagecollection;
 
 public class GarbageCollectionDemo {
 
+    int objectId;
 
-    public GarbageCollectionDemo() {
-        System.out.println(this + " Garbage collection demo");
+    public GarbageCollectionDemo(int objectId) {
+        this.objectId = objectId;
+        System.out.println("Created " + this.objectId);
     }
 
     public static void main(String[] args) {
 
         //below we are creating objects but not assigning so they are unreachable.
-        new GarbageCollectionDemo();
-        new GarbageCollectionDemo();
-        new GarbageCollectionDemo();
+        //force JVM to initiate garbage collection by creating a lot of unreachable objects.
+        for (int i = 0; i <= 500000; i++) {
+            new GarbageCollectionDemo(i);
+        }
     }
 
     @Override
     protected void finalize() throws Throwable {
+        System.out.println("Invoked finalized method for " + this.objectId);
         System.gc();
     }
 }
