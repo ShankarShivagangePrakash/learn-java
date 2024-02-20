b3e0a9f55cc4e1ec13aace4d3c31d32ce15fe66b
ShankarShivagangePrakash <s.p.shankar.na@gmail.com>
2024-02-20 22:45:44 +0530

    custom annotation

---
+++ src/annotations/customannotation/MyAnnotationTest.java
@@ -0,0 +1,9 @@
+package annotations.customannotation;
+
+public class MyAnnotationTest {
+
+        // custom annotation applied successfully.
+        @MyAnnotation
+        void myMethod() {
+        }
+}
