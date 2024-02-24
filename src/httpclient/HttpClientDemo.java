140ed2b91c6c76e763ebb9a86121708810ad55c0
ShankarShivagangePrakash <s.p.shankar.na@gmail.com>
2024-02-24 11:19:21 +0530

    get response headers

--- src/httpclient/HttpClientDemo.java
+++ src/httpclient/HttpClientDemo.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Map;
 
 public class HttpClientDemo {
 
@@ -47,9 +50,18 @@ public class HttpClientDemo {
         // making HTTP call by passing request and specifying the response type.
         HttpResponse<String> postApiResponse = httpClient.send(postRequest, HttpResponse.BodyHandlers.ofString());
 
+        // we can also get the response header
+        // headers will come as a map
+        Map<String, List<String>> responseHeaderMap = postApiResponse.headers().map();
+
         // parsing the response object.
         System.out.println("\n\n POST API response\n\n");
         System.out.println("Response code: " + postApiResponse.statusCode());
         System.out.println("Response body:\n" + postApiResponse.body());
+
+        System.out.println("\nResponse headers");
+        for (var  entry : responseHeaderMap.entrySet()) {
+            System.out.println(entry.getKey() + " ----> " + entry.getValue());
+        }
     }
 }
