e443679bd6fd6f13015bb519d55d9c9f3ee2c2d9
ShankarShivagangePrakash <s.p.shankar.na@gmail.com>
2024-02-24 15:07:26 +0530

    more concepts on records

--- src/java13and14features/recordfeature/Employee.java
+++ src/java13and14features/recordfeature/Employee.java
@@ -9,4 +9,28 @@ package java13and14features.recordfeature;
      Record is a class which provides all those functionalities.
  */
 public record Employee(String name, double salary) {
+
+    // We cannot create member variables for object of this class. We are allowed to create only staic variables.
+    // If we need a member variable for object, you need to modify line 11.
+    static int staticMember = 10;
+
+    // Default constructor created by us.
+    public Employee() {
+        this("Default Employee", 500);
+    }
+
+    // parameterized constructor created by us.
+    /*
+         In both default and parameterized constructor, it is our responsibility to initialize
+         object member variables i.e. name and salary
+         because by default they are final in Record.
+     */
+    public Employee(String name) {
+        this(name, 1000);
+    }
+
+    // This can be static or non-static
+    public static String getRecordName() {
+        return "Employee Record";
+    }
 }
