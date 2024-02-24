5b8d5f0c161ab9629afee824624c9df12232f550
ShankarShivagangePrakash <s.p.shankar.na@gmail.com>
2024-02-24 09:35:47 +0530

    chess game characters support using unicode characters

---
+++ src/java12features/unicodecharacters/Chess.java
@@ -0,0 +1,15 @@
+package java12features.unicodecharacters;
+
+public class Chess {
+
+    public static void main(String[] args) {
+
+        // In java 12 unicode characters representing chess game pawns are added
+
+        System.out.println("Chess game black king: \u265A");
+        System.out.println("Chess game black Queen: \u265B");
+
+        System.out.println("Chess game White King: \u2654");
+        System.out.println("Chess game White Queen: \u2655");
+    }
+}
